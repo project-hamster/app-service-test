1fab624534aba7a760161f9f208933b46d4a367f
project hamster <73212893+project-hamster@users.noreply.github.com>
2023-05-02 23:52:58 +0900

    query

--- application.py
+++ application.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
-def chat():
+def chat(query):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": "テスラについて教えて"}
+            {"role": "user", "content": query}
         ]
     )
 
@@ -19,7 +19,8 @@ def chat():
 @app.route("/", methods=["GET", "POST"])
 def main_page():
     if request.method == 'GET':
-        return chat()
+        query = request.args.to_dict()['q']
+        return chat(query)
     elif request.method == 'POST':
         return "POST is success!"
 
